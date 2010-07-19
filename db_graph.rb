6e0697bec9b9da4e070d5a8e0b78f4a2da40ead2
Roosevelt <roosevelt1@gmail.com>
2010-07-19 16:20:20 -0300

    primeira versão do script de migração do banco de dados dos gráficos

---
+++ db_graph.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'mysql'
+require 'json'
+
+user= 'root'
+pass= ''
+host= 'localhost'
+db_name='holmes'
+
+mysql_db = Mysql.new(host, user, pass)
+mysql_db.select_db(db_name)
+select_result = mysql_db.query('SELECT widget_id,json_config, type FROM widget')
+
+update_counter = 0
+delete_counter = 0
+
+puts 'Widget table backup started...'
+
+mysqldump = IO.popen("mysqldump -u -p holmes widget > widget.sql")
+Process.waitpid(mysqldump.pid)
+
+puts "Widget table backup finished. widget.sql backup file was created into the directory this script was executed."
+
+while row = select_result.fetch_row do    
+    json_config = JSON row[1]  
+
+    chart_type = json_config['chart']      
+
+    json_config.delete('chart')    
+    json_config.delete('behaviour')                
+    json_config.delete('numEventsToDisplay')
+    json_config['labelType'] = 'dynamic'  
+    json_str = JSON.dump(json_config)
+
+    case chart_type
+	    when 'column'       
+            update_res = mysql_db.query("UPDATE widget SET json_config='" + json_str + "', type='ExtFlotBarChart' WHERE widget_id=" + row[0])
+            update_counter+=1
+	    when 'pie-raphaeljs'
+            update_res = mysql_db.query("UPDATE widget SET json_config='" + json_str + "', type='ExtFlotPieChart' WHERE widget_id=" + row[0])
+            update_counter+=1
+        when 'line'
+            update_res = mysql_db.query("UPDATE widget SET json_config='" + json_str + "', type='ExtFlotLineChart' WHERE widget_id=" + row[0])
+            update_counter+=1
+        when 'dataview'
+            delete_result = mysql_db.query("DELETE FROM widget WHERE widget_id=" + row[0])
+            delete_counter+=1
+        else       
+	    end 
+
+    if row[2] == 'LineChart'
+        json_config.delete('numDivisions') 
+        json_config['labelType'] = 'static'
+        json_str = JSON.dump(json_config)
+        update_res = mysql_db.query("UPDATE widget SET json_config='" + json_str + "', type='ExtFlotLineChart' WHERE widget_id=" + row[0])
+        update_counter+=1
+    end
+            
+end
+
+puts String(update_counter) + ' updated rows, ' + String(delete_counter) + ' removed rows'  
+
+select_result.free
+mysql_db.close
